6761364abe346b4e4d4bcc830c38a838c1603df0
Amogh Thusoo <98604199+amoghthusoo@users.noreply.github.com>
2022-03-03 10:18:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+from ui_home import screens
+
+Window.size = (1366/3.4,750)
+
+class HomeScreen(Screen):
+    pass
+
+class DeveloperScreen(Screen):
+    pass
+
+class InfoScreen(Screen):
+    pass
+
+class HelpScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(HomeScreen(name = 'home'))
+sm.add_widget(InfoScreen(name = 'info'))
+
+class BMI_Calc(MDApp):
+
+    def build(self):
+        self.i = 0
+        self.theme_cls.primary_palette = 'Purple'
+        self.app = Builder.load_string(screens)
+        return self.app 
+
+    def callback_calculate(self, _name, weight, height):
+
+        def bmi_status(bmi):
+
+            if bmi < 18.5:
+                status = 'You are Underweight.'
+            elif 18.5 <= bmi <= 24.9:
+                status = 'You are Healthy.'
+            elif 25 <= bmi <=29.9:
+                status = 'You are Overweight.'
+            else:
+                status = 'You are Obese.'
+
+            return status
+        
+        self._name = _name
+        self.weight = weight
+        self.height = height
+
+        self.close_button = MDFlatButton(text = 'OK', on_release = self.callback_dialog_close)
+        self.dialog_window = MDDialog(size_hint = (0.9, 1), buttons = [self.close_button])
+        
+        if self._name == '' or self.weight == '' or self.height == '':
+            self.dialog_window.title = 'INVALID INPUT'
+            self.dialog_window.open()
+        
+        else:
+            try:
+    
+                self.bmi = float(self.weight)/float(self.height)**2
+
+                self.status = bmi_status(self.bmi)
+                print(self.status)
+
+                self.dialog_window.title = 'Hi ' + self._name + ', Your BMI is :'
+                self.dialog_window.text = '\n\n' + str(round(self.bmi, 3)) + '\n'  + self.status
+                
+                self.dialog_window.open()            
+            except:
+                self.dialog_window.title = 'INVALID INPUT'
+                self.dialog_window.open()
+
+    def callback_dialog_close(self, obj):
+        self.dialog_window.dismiss()
+
+    def callback_help(self):
+        self.root.transition.direction = 'left'
+        self.root.current = 'help'
+
+    def callback_return_home(self):
+        self.root.transition.direction = 'right'
+        self.root.current = 'home'
+
+    def callback_change_theme(self):
+        self.colour_list = ['Blue', 'Pink', 'Yellow', 'Green', 'Orange', 'DeepPurple', 'LightBlue', 'Red', 'Indigo', 'LightGreen', 'DeepOrange', 'Cyan', 'Lime', 'Teal', 'Amber', 'BlueGray', 'Purple']
+        self.theme_cls.primary_palette = self.colour_list[self.i]
+        #print(self.colour_list[self.i])
+        if self.i < len(self.colour_list) - 1:
+            self.i += 1
+        else:
+            self.i = 0
+
+root = BMI_Calc()
+if __name__ == '__main__':
+    root.run()
---
+++ ui_home.py
@@ -0,0 +1,303 @@
+screens = '''
+
+ScreenManager:
+    HomeScreen:
+    DeveloperScreen:
+    InfoScreen:
+    HelpScreen:
+    
+        
+
+<HomeScreen>:
+
+    name : 'home'
+    
+    Screen:
+        
+        MDNavigationLayout:
+            
+            ScreenManager:
+                
+                Screen:
+                    
+                    BoxLayout:
+
+                        orientation : 'vertical'
+                        spacing : '20dp'
+
+                        MDToolbar:
+                            title : 'BMI Analyzer'
+                            left_action_items : [['menu', lambda x: nav_drawer.set_state('toggle')]]
+                            right_action_items : [['dots-vertical', lambda x: nav_drawer.set_state('toggle') ]]
+                            elevation : 10
+                            pos_hint : {'center_y' : 0.96}
+                        
+                        
+
+                        MDTextField:
+                            id : person_name
+                            hint_text : 'Enter Your Name'
+                            helper_text : ''
+                            helper_text_mode : 'on_focus'
+                            icon_right : 'account'
+                            icon_right_color : app.theme_cls.primary_color
+                            size_hint_x : None
+                            width : '300dp'
+                            pos_hint : {'center_x' : 0.5, 'center_y' : 0.80}
+
+                                
+                        
+                        MDTextField:
+                            id : person_weight
+                            hint_text : 'Enter Your Weight'
+                            helper_text : 'in kilograms'
+                            helper_text_mode : 'on_focus'
+                            icon_right : 'weight'
+                            icon_right_color : app.theme_cls.primary_color
+                            size_hint_x : None
+                            width : '300dp'
+                            pos_hint : {'center_x' : 0.5, 'center_y' : 0.70}
+
+                        MDTextField:
+                            id : person_height
+                            hint_text : 'Enter Your Height'
+                            helper_text : 'in meters'
+                            helper_text_mode : 'on_focus'
+                            icon_right : 'arrow-up'
+                            icon_right_color : app.theme_cls.primary_color
+                            size_hint_x : None
+                            width : '300dp'
+                            pos_hint : {'center_x' : 0.5, 'center_y' : 0.60}
+                                  
+                        MDFillRoundFlatButton:
+                            text : 'Calculate'
+                            font_size : '18sp'
+                            size_hint_y : None
+                            height : '40dp'
+                            pos_hint : {'center_x' : 0.5, 'center_y' : 0.50}
+                            on_release : app.callback_calculate(person_name.text, person_weight.text, person_height.text)
+
+                      
+                        MDBottomAppBar:
+                            MDToolbar :
+                                title : 'Help'
+                                icon : 'information-variant'
+                                type : 'bottom'
+                                left_action_items : [['help-box', lambda x: app.callback_help()]]
+                                mode : 'end'
+                                
+                                on_action_button : 
+                                    root.manager.transition.direction = 'left'
+                                    root.manager.current = 'info'
+
+                        BoxLayout:
+                            
+                            orientation : 'vertical'
+
+                            
+                            MDFillRoundFlatButton:
+                                text : 'Change Theme'
+                                font_size : '18sp'
+                                pos_hint : {'center_x' : 0.5, 'center_y' : 0.50}
+                                on_release : app.callback_change_theme()
+                                  
+                                
+                        Widget:
+                
+            MDNavigationDrawer:
+                id : nav_drawer
+                
+                MDBoxLayout:
+                    orientation : 'vertical'
+                    padding : '10dp'
+
+                    MDLabel:
+                        text : 'BMI Analyzer'
+                        color : app.theme_cls.primary_color
+                        halign : 'center'
+                        font_style : 'H4'
+                        size_hint_y : None
+                        height : self.texture_size[1]
+
+                    Widget:
+                        size_hint : (1, 0.05)
+
+                    MDList:
+
+                        
+                        OneLineIconListItem : 
+                            text : 'Developer'
+                            on_release:
+                                root.manager.transition.direction = 'left'
+                                root.manager.current = 'developer'
+
+                            IconLeftWidget:
+                                icon : 'android'
+                            
+                        OneLineIconListItem : 
+                            text : 'Calculate'
+                            on_release:
+                                app.callback_calculate(person_name.text, person_weight.text, person_height.text)
+                            IconLeftWidget:
+                                icon : 'play' 
+                        
+                        OneLineIconListItem : 
+                            text : 'Info'
+                            on_release:
+                                root.manager.transition.direction = 'left'
+                                root.manager.current = 'info'
+                            IconLeftWidget:
+                                icon : 'information-variant'
+                        
+                        OneLineIconListItem : 
+                            text : 'Help'
+                            on_release:
+                                root.manager.transition.direction = 'left'
+                                root.manager.current = 'help'
+                            IconLeftWidget:
+                                icon : 'help'
+
+                    Widget:
+                    
+        
+<DeveloperScreen>:
+
+    name : 'developer'
+    
+    MDBoxLayout:
+
+        orientation : 'vertical'
+        spacing : '15dp'
+        
+        MDToolbar:
+            title : 'Developer'
+            left_action_items : [['arrow-left', lambda x: app.callback_return_home()]]
+            elevation : 10
+            pos_hint : {'center_y' : 0.96}
+            
+
+        MDLabel:
+            text : 'Application developed by Amogh Thusoo.'
+            #size_hint_y : None
+            #height : self.texture_size[1]
+            size_hint_x : None
+            size_hint_y : None
+            width : '340dp'
+            height : self.texture_size[1]
+            halign : 'justify'
+            pos_hint : {'center_x' : 0.47}
+
+        MDLabel:
+            text : '© 2022 Amogh Thusoo'
+            #size_hint_y : None
+            #height : self.texture_size[1]
+            size_hint_x : None
+            size_hint_y : None
+            width : '340dp'
+            height : self.texture_size[1]
+            halign : 'justify'
+            pos_hint : {'center_x' : 0.47}
+            
+
+        Widget:
+
+<InfoScreen>:
+
+    name : 'info'
+
+    MDScreen:
+        MDBoxLayout:
+
+            orientation : 'vertical'
+            spacing : '15dp'
+            
+            MDToolbar:
+                title : 'Information'
+                left_action_items : [['arrow-left', lambda x: app.callback_return_home()]]
+                elevation : 10
+                pos_hint : {'center_y' : 0.96}
+
+            
+
+            MDLabel:
+                text : "This is a simple BMI (Body Mass Index) Analyzer used to determine the person's BMI and categorize him/her as Underweight, Healthy, Overweight or Obese. Enter your name, weight and height to calculate your BMI and check your category!"
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+                
+            MDLabel:
+                text : '© 2022 Amogh Thusoo'
+                #size_hint_y : None
+                #height : self.texture_size[1]
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+
+            Widget:
+
+
+<HelpScreen>:
+
+    name : 'help'
+
+    MDScreen:
+        MDBoxLayout:
+
+            orientation : 'vertical'
+            spacing : '15dp'
+            
+            MDToolbar:
+                title : 'Help'
+                left_action_items : [['arrow-left', lambda x: app.callback_return_home()]]
+                elevation : 10
+                pos_hint : {'center_y' : 0.96}
+
+            
+
+            MDLabel:
+                text : "1. Enter your name, weight and height in the space provided."
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+            
+            MDLabel:
+                text : "2. Remember that you have to provide weight in kilograms and height in meters."
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+
+            MDLabel:
+                text : "3. Click on Calculate button at the bottom or click the Calculate button from the menu bar."
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+                
+            MDLabel:
+                text : '   © 2022 Amogh Thusoo'
+                #size_hint_y : None
+                #height : self.texture_size[1]
+                size_hint_x : None
+                size_hint_y : None
+                width : '340dp'
+                height : self.texture_size[1]
+                halign : 'justify'
+                pos_hint : {'center_x' : 0.47}
+
+            Widget:  
+'''          
+
